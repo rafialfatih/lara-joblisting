ed61764fd40cf6a53af5b6558fd98cd2cc8b0f30
rafi <rafi.alfatih@gmail.com>
2022-08-01 07:44:01 +0700

    Modified listings table

--- database/migrations/2022_07_27_040050_create_listings_table.php
+++ database/migrations/2022_07_27_040050_create_listings_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -15,6 +16,7 @@ return new class extends Migration {
         Schema::create("listings", function (Blueprint $table) {
             $table->id();
             $table->string("title");
+            $table->string("logo")->nullable();
             $table->string("tags");
             $table->string("company");
             $table->string("location");

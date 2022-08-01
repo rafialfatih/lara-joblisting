b447a981c104ce542c4b049c076d91415ef3ade4
rafi <rafi.alfatih@gmail.com>
2022-08-01 07:43:19 +0700

    Add User Controller

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,6 +9,17 @@ class Listing extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'logo',
+        'company',
+        'location',
+        'website',
+        'email',
+        'tags',
+        'description',
+    ];
+
     public function scopeFilter($query, array $filters)
     {
         if ($filters['tag'] ?? false) {

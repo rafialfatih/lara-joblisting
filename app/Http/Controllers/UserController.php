3a3ef414ccac9e37e232748aba04303f88971f6b
rafi <rafi.alfatih@gmail.com>
2022-08-01 07:50:44 +0700

    Add create function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class UserController extends Controller
 {
-    //
+    public function create()
+    {
+        return view("users.register");
+    }
 }

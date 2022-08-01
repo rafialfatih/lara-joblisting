3ae4cc7c880b07c4b72e85b5ac35e5b9469706e4
rafi <rafi.alfatih@gmail.com>
2022-08-01 10:06:24 +0700

    Add user registration

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,12 +26,29 @@
   <nav class="flex justify-between items-center mb-4">
   <a href="/"><img class="w-24" src={{ asset("images/logo.png") }} alt="" class="logo"/></a>
     <ul class="flex space-x-6 mr-6 text-lg">
-      <li>
-        <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
-      </li>
-      <li>
-        <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>Login</a>
-      </li>
+      @auth
+        <li>
+          <span class="font-bold">Welcome, {{ auth()->user()->name }}!</span>
+        </li>
+        <li>
+          <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-gear"></i>Manage Listings</a>
+        </li>
+        <li>
+          <form method="POST" class="inline" action="/logout">
+            @csrf
+            <button type="submit">
+              <i class="fa-solid fa-door-closed"></i> Logout
+            </button>
+          </form>
+        </li>
+      @else
+        <li>
+          <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+        </li>
+        <li>
+          <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>Login</a>
+        </li>
+      @endauth
     </ul>
   </nav>
 
